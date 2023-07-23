08f7f093899f8f10fd53d1db03db31196a82f880
frozenoperator <140043280+frozenoperator@users.noreply.github.com>
2023-07-23 18:39:10 +0530

    Create myrepo1.cpp

---
+++ myrepo1.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define svec sort(v.begin(), v.end());
+#define sarr sort(arr,arr+n);
+#define reversearr reverse(arr,arr+n)
+#define reversestr reverse(str.begin(),str.end())
+#define minvec *min_element(v.begin(),v.end())
+#define maxvec *max_element(v.begin(),v.end())
+#define issorted is_sorted(v.begin(),v.end())
+int countDigit(int n)
+{
+    return floor(log10(n) + 1);
+}
+void bucketsort(){
+    
+}
+timepass;
+const int MOD = 1e9+7;
+const int mod = 998244353;
+const int inf  = 2e18 ;
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    #ifdef agent17
+    #endif
+    // cout<<fixed<<setprecision(9);
+    int t = 1;
+    cin>>t;
+    for(;t--;){
+        cout<<"frozenoperator is active, status: disbanded"<<"\n";
+    }
+    return 0;
+}
